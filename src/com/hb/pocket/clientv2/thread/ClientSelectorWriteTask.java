18a795979b47691de657a1dea9707b4d000e9e98
Robin.Han <hbinboy@gmail.com>
2018-09-07 17:30:27 +0800

    Add write function that send message with header.

--- src/com/hb/pocket/clientv2/thread/ClientSelectorWriteTask.java
+++ src/com/hb/pocket/clientv2/thread/ClientSelectorWriteTask.java
@@ -1,6 +1,9 @@
 package com.hb.pocket.clientv2.thread;
 
 import com.hb.pocket.clientv2.thread.callback.IClientSelectorWriteCallback;
+import com.hb.pocket.data.DataManager;
+import com.hb.utils.config.ClientConfig;
+import com.hb.utils.log.MyLog;
 
 import java.io.IOException;
 import java.net.Socket;
@@ -52,6 +55,21 @@ public class ClientSelectorWriteTask implements Runnable {
      * @throws IOException
      */
     private boolean write(SocketChannel channel, String msg) throws IOException {
+        if (ClientConfig.writeDataWithHeader) {
+            return writeDataWithHeader(channel, msg);
+        } else {
+            return writeRawData(channel, msg);
+        }
+    }
+
+    /**
+     * Process the write message without Header {@link com.hb.pocket.data.header.Header}
+     * @param channel
+     * @param msg
+     * @return
+     * @throws IOException
+     */
+    private boolean writeRawData(SocketChannel channel, String msg) throws IOException {
         try {
             String[] tmpArray = msg.split("\n");
             ByteBuffer[] bufferArray = new ByteBuffer[tmpArray.length];
@@ -69,6 +87,36 @@ public class ClientSelectorWriteTask implements Runnable {
                 channel.close();
             }
         }
+        return false;
+    }
+
+    /**
+     * Process the write message with Header {@link com.hb.pocket.data.header.Header}
+     * @param channel
+     * @param msg
+     * @return
+     * @throws IOException
+     */
+    private boolean writeDataWithHeader(SocketChannel channel, String msg) throws IOException {
+        DataManager dataManager = new DataManager();
+        String wholeMessageMD5 = dataManager.md5(msg);
+        String[] result = dataManager.spliteString(msg, 7);
+
+        try {
+            for (int i = 0; i < result.length; i++) {
+                byte[] data = dataManager.genSendDataPackage(result[i] + '\n', i, result.length, wholeMessageMD5);
+                ByteBuffer buffer = ByteBuffer.allocate(data.length); // Alloc heap buffer.
+                buffer.put(data);
+                buffer.flip();// Switch the read model.
+                MyLog.d(TAG, "" + channel.write(buffer/*, 0, bufferArray.length*/));
+            }
+            return true;
+        } catch (IOException e) {
+            if (channel != null) {
+                channel.close();
+            }
+        }
+
         return false;
     }
 }
